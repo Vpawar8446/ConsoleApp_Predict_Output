55bf4625aacbf5576e81d3d9373c2528968d7000
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:37:43 +0530

    Add project files.

---
+++ ConsoleApp_Predict_Output/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp_Predict_Output
+{
+    class Car
+    {
+        public int Model;
+        public Car(int a)
+        {
+            Model = a;
+        }
+        public void Display_Model()
+        {
+            Console.WriteLine(Model);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Car Ford = new Car(22);
+                Ford.Model = 10;
+                Car BMW = Ford;
+                BMW.Display_Model(); //10
+                Ford.Display_Model(); //10
+            
+
+        }
+    }
+}
